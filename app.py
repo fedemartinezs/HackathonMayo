b846f77f04c6e8f50250aa12491e335b2a26a2ef
Pedro H <pedroherman@92gmail.com>
2022-05-31 14:55:08 -0400

    codigo de dataframe

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import pandas as pd
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    df = pd.read_csv('datos.txt', parse_dates=['fecha'], dayfirst=True)
+    df.to_string()
+
+    def parse_to_dicc(datos):
+        diccionary = datos.to_dict(orient='records')
+        print(diccionary)
+
+    parse_to_dicc(df)
+    
     return render_template("mostrar_datos.html")
 #holaaa
 
